6deb5ecc4b0ad58cfd2c481048bb3dbd180df048
Jeff <jeffersonvarela.jv@hotmail.com>
2021-09-27 22:39:47 -0600

    boton de borrar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
-import { PopoverController } from '@ionic/angular';
+import { ActionSheetController, PopoverController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { UsuariosService } from '../service/usuarios.service';
 import { PopInfoComponent } from '../components/pop-info/pop-info.component';
@@ -18,7 +18,8 @@ export class HomePage {
 
   constructor(private router: Router,
     private usuarioServise: UsuariosService,
-    private popCtrl: PopoverController) {
+    private popCtrl: PopoverController,
+    public actionSheetCrl: ActionSheetController) {
 
 
 
@@ -28,20 +29,19 @@ export class HomePage {
   }
 
   /* funcion del pop */
-  async mostrarPop(event){
+ /*  async mostrarPop(event){
     const popover = await this.popCtrl.create({
       component: PopInfoComponent,
       event: event,
       mode: 'ios',
-     // backdropDismiss: false
+    
 
     });
 
     await popover.present()
     const {data} = await popover.onWillDismiss();  //se ejecuta antes de cerrarce
     console.log('padre:', data);
-
-  }
+  } */
 
   getUsuario() {
     this.usuarioServise.getUsuario().subscribe(data => {
@@ -70,6 +70,43 @@ export class HomePage {
 
   addUser() {
     this.router.navigate(['/add-user']);
+  }
+
+  /* --------------------------------------------- */
+  async presentActionSheet(id: string) {
+    const actionSheet = await this.actionSheetCrl.create({
+      header: 'Albums',
+      //backdropDismiss: false, /* para que no se cancele el action sheet al tocar la pantalla */
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        cssClass: 'rojo', /* css personalizada para color rojo, creada en global.css */
+        handler: () => {
+          //console.log('Delete clicked');
+          this.eliminarUsuario(id)
+        }
+      }, {
+        text: 'Editar',
+        icon: 'pencil-outline',
+        handler: () => {
+          console.log('Editar clicked');
+        }
+      }, 
+      {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
 }
